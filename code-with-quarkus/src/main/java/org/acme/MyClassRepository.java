2906a74d7dadb0b4bcdd4f4feff654fae918ae94
KrasilnikovAnatoly <153846211+KrasilnikovAnatoly@users.noreply.github.com>
2024-04-16 11:18:04 +0300

    Delete code-with-quarkus directory

--- code-with-quarkus/src/main/java/org/acme/MyClassRepository.java
+++
@@ -1,8 +0,0 @@
-package org.acme;
-
-import jakarta.enterprise.context.ApplicationScoped;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-
-@ApplicationScoped
-public class MyClassRepository implements PanacheRepository<MyClass>{
-}
